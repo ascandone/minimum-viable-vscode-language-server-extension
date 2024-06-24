04cd31148b5cb0bfef5b6364bad18954fbc2947f
Alessandro Scandone <alescandone@gmail.com>
2024-06-24 20:23:41 +0200

    responding to initialize

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,57 @@
 import { write } from "./log";
 import { MessageBuffer } from "./message-buffer";
-import { Message } from "./protocol";
+import * as dec from "ts-decode";
 
 const msgBuf = new MessageBuffer();
 
+function initialize(): object {
+  return {
+    capabilities: {},
+    serverInfo: {
+      name: "lsp-from-scratch",
+      version: "0.0.1",
+    },
+  };
+}
+
+const requestMsgParser = dec.object({
+  method: dec.string.required,
+  id: dec.number.optional,
+});
+
+type RespondMethod = (msg: unknown) => object;
+
+const methods: Record<string, RespondMethod> = {
+  initialize,
+};
+
 process.stdin.on("data", (chunk) => {
-  write(chunk.toString());
-  const out = msgBuf.receiveData(chunk);
+  const receivedMsg = msgBuf.receiveData(chunk);
+  write({ receivedMsg });
+
+  if (receivedMsg === undefined) {
+    return;
+  }
+
+  const result = requestMsgParser.decode(receivedMsg);
+  if (result.error) {
+    return;
+  }
+
+  const parsedRequestMsg = result.value;
+
+  const method = methods[parsedRequestMsg.method];
+  if (method === undefined || parsedRequestMsg.id === undefined) {
+    // TODO handle err
+    return;
+  }
+
+  const outMsg = JSON.stringify({
+    id: parsedRequestMsg.id,
+    result: method(parsedRequestMsg),
+  });
+
+  const len = Buffer.byteLength(outMsg, "utf-8");
+
+  process.stdout.write(`Content-Length: ${len}\r\n\r\n${outMsg}`);
 });
